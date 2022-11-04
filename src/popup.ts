d1e3c016931219f97549cb5e76fad056de5819dd
--get <67471674+ninebolt6@users.noreply.github.com>
2022-11-04 18:07:24 +0900

    feat: download data as json

--- src/popup.ts
+++ src/popup.ts
@@ -1,3 +1,14 @@
+interface AdWatchData {
+  watchStartedAt: number;
+  totalTimePassed: number;
+  timePaused: number;
+  timeWatched: number;
+}
+
+interface StorageData {
+  [key: number]: AdWatchData;
+}
+
 const showValue = async () => {
   const msg = await chrome.storage.local.get();
   console.log(msg);
@@ -8,10 +19,33 @@ const clearValue = async () => {
   console.log("cleared");
 };
 
+const downloadData = async () => {
+  const data: StorageData = await chrome.storage.local.get();
+  const jsonData = JSON.stringify(data);
+  const blob = new Blob([jsonData], { type: "text/plain" });
+  const url = URL.createObjectURL(blob);
+
+  const anchor = document.createElement("a");
+  anchor.setAttribute("href", url);
+  anchor.setAttribute("download", "log.txt");
+
+  const mouseEvent = new MouseEvent("click", {
+    bubbles: true,
+    cancelable: true,
+    view: window,
+  });
+
+  anchor.dispatchEvent(mouseEvent);
+};
+
 document.addEventListener("DOMContentLoaded", function () {
   document.querySelector(".show")!.addEventListener("click", showValue);
 });
 
 document.addEventListener("DOMContentLoaded", function () {
   document.querySelector(".clear")!.addEventListener("click", clearValue);
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  document.querySelector(".download")!.addEventListener("click", downloadData);
 });
