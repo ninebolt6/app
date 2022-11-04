403c5b91f8ed064de5d1074e2ba055355e506df4
--get <67471674+ninebolt6@users.noreply.github.com>
2022-11-04 14:29:31 +0900

    feat: debug menu

---
+++ dist/popup.js
@@ -0,0 +1,17 @@
+const showValue = async () => {
+  const msg = await chrome.storage.local.get("data");
+  console.log(msg);
+};
+
+const clearValue = async () => {
+  await chrome.storage.local.clear();
+  console.log("cleared");
+};
+
+document.addEventListener("DOMContentLoaded", function () {
+  document.querySelector(".show").addEventListener("click", showValue);
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  document.querySelector(".clear").addEventListener("click", clearValue);
+});
